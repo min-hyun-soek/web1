9b7e61dae4ed45002fd8049205b05c01d9ce3778
min-hyun-soek <160083563+min-hyun-soek@users.noreply.github.com>
2024-02-15 10:45:38 +0900

    Add files via upload

---
+++ BuyMapper.java
@@ -0,0 +1,10 @@
+package kr.co.mapper;
+
+import java.util.List;
+
+import kr.co.domain.BuyDTO;
+
+public interface BuyMapper {
+   public abstract List<BuyDTO> getList(Long m_no);
+   public abstract void insertSelectkey(BuyDTO buy);
+}
